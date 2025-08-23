f90b39ea3e18838fc45168f94b9299cbe7a38b61
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2025-08-23 15:42:17 +0700

    Initial commit

---
+++ src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend, { type HttpBackendOptions } from 'i18next-http-backend';
+import { DateTime } from 'luxon';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init<HttpBackendOptions>({
+    backend: {
+      loadPath: '/locales/{{lng}}/common.json',
+    },
+    debug: true,
+    fallbackLng: 'ru',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+i18n.services?.formatter?.add('DATE_HUGE', (value, lng = 'ru') => {
+  return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime.DATE_HUGE);
+});
+
+export default i18n;
