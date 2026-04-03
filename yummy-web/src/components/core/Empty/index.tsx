f84d5dbe6ff8edb45eb9dc4aebd0733a00f5375b
Alexandr Panteleev <panteleev@brooma.ru>
2026-04-03 12:09:12 +0700

    add empty image

--- yummy-web/src/components/core/Empty/index.tsx
+++ yummy-web/src/components/core/Empty/index.tsx
@@ -1,11 +1,30 @@
+import NotFoundIcon from '@icons/NotFoundIcon';
 import './index.scss';
-import { Empty as AntdEmpty } from 'antd';
+import type { ButtonProps } from 'antd';
+import { Empty as AntdEmpty, Button, Flex, Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { token } from '@theme/token';
 
-function Empty() {
+interface IProps {
+  description?: string;
+  buttonProps?: ButtonProps;
+}
+
+function Empty({ description, buttonProps }: IProps) {
   const { t } = useTranslation();
-
-  return <AntdEmpty image={AntdEmpty.PRESENTED_IMAGE_SIMPLE} description={t('dataNotFound')} />;
+  return (
+    <AntdEmpty
+      className="empty"
+      styles={{ image: { height: 200 } }}
+      image={<NotFoundIcon />}
+      description={
+        <Flex align="center" gap={token.marginXXS} justify="center">
+          <Typography.Text type="secondary">{description ?? t('dataNotFound')}</Typography.Text>
+          {buttonProps && <Button {...buttonProps} />}
+        </Flex>
+      }
+    />
+  );
 }
 
 export default Empty;
