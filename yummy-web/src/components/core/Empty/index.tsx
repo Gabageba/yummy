7fb8844842d0425776ad1f517a61290ba262e0c3
Alexandr Panteleev <panteleev@brooma.ru>
2026-04-03 12:28:27 +0700

    clean empty

--- yummy-web/src/components/core/Empty/index.tsx
+++ yummy-web/src/components/core/Empty/index.tsx
@@ -1,16 +1,13 @@
 import NotFoundIcon from '@icons/NotFoundIcon';
 import './index.scss';
-import type { ButtonProps } from 'antd';
-import { Empty as AntdEmpty, Button, Flex, Typography } from 'antd';
+import { Empty as AntdEmpty, Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { token } from '@theme/token';
 
 interface IProps {
   description?: string;
-  buttonProps?: ButtonProps;
 }
 
-function Empty({ description, buttonProps }: IProps) {
+function Empty({ description }: IProps) {
   const { t } = useTranslation();
   return (
     <AntdEmpty
@@ -18,10 +15,7 @@ function Empty({ description, buttonProps }: IProps) {
       styles={{ image: { height: 200 } }}
       image={<NotFoundIcon />}
       description={
-        <Flex align="center" gap={token.marginXXS} justify="center">
-          <Typography.Text type="secondary">{description ?? t('dataNotFound')}</Typography.Text>
-          {buttonProps && <Button {...buttonProps} />}
-        </Flex>
+        <Typography.Text type="secondary">{description ?? t('dataNotFound')}</Typography.Text>
       }
     />
   );
